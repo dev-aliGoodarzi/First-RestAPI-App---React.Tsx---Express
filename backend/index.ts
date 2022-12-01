27f3c10e93b288f2e2fffe8b61bfdcb78ed263fb
M-r-hydra <me74242@gmail.com>
2022-12-02 00:24:10 +0330

    some clean code !

--- backend/index.ts
+++ backend/index.ts
@@ -1,20 +1,33 @@
-import { I_Movie } from "./_src/Models/interface";
-import { sendAllMovies } from "./_src/Services/sendAllMoviesService";
+// Express
 import express, { Express, Request, Response } from "express";
-import { getAllMovieService } from "./_src/Services/getAllMovieService";
+// Express
+// Services
+import { sendAllMovies } from "./_src/Services/sendAllMoviesService";
 import { selectMovieByIdService } from "./_src/Services/selectMovieByIdService";
+// Services
 
+// initialize The Express
 const app: Express = express();
+// initialize The Express
 
+// Setting Up Middlewares
+// ****** for support json Format !
 app.use(express.json());
+// ****** for support json Format !
+// Setting Up Middlewares
 
+// ******************************* Routes
+// Get
 app.get("/api/movies", (req: Request, res: Response) => {
   sendAllMovies(req, res);
 });
-
 app.get("/api/movies/:movieId", (req: Request, res: Response) => {
   selectMovieByIdService(req, res);
 });
+// Post
+// Put
+// Delete
+// ******************************* Routes
 
 const port: number | string = process.env.customPort || 3100;
 app.listen(Number(port), () => {
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,17 +3,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const sendAllMoviesService_1 = require("./_src/Services/sendAllMoviesService");
+// Express
 const express_1 = __importDefault(require("express"));
+// Express
+// Services
+const sendAllMoviesService_1 = require("./_src/Services/sendAllMoviesService");
 const selectMovieByIdService_1 = require("./_src/Services/selectMovieByIdService");
+// Services
+// initialize The Express
 const app = (0, express_1.default)();
+// initialize The Express
+// Setting Up Middlewares
+// ****** for support json Format !
 app.use(express_1.default.json());
+// ****** for support json Format !
+// Setting Up Middlewares
+// ******************************* Routes
+// Get
 app.get("/api/movies", (req, res) => {
     (0, sendAllMoviesService_1.sendAllMovies)(req, res);
 });
 app.get("/api/movies/:movieId", (req, res) => {
     (0, selectMovieByIdService_1.selectMovieByIdService)(req, res);
 });
+// Post
+// Put
+// Delete
+// ******************************* Routes
 const port = process.env.customPort || 3100;
 app.listen(Number(port), () => {
     console.log(`Server Is Running On Port ${port}`);
