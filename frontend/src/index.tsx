17f5139e29edaa6f3a7504db48631689ce489781
hydra <me74242@gmail.com>
2022-12-03 03:10:43 +0330

    install react-router-dom

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,25 @@
+// React
 import React from "react";
 import ReactDOM from "react-dom/client";
+// React
+// App Component
 import App from "./App";
+// App Component
+// Main & Root Css File
 import "./index.css";
+// Main & Root Css File
+// Modules
+import { BrowserRouter as Router } from "react-router-dom";
+// Modules
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
